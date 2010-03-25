eefb61de4c8aa8fad0e5a62800a6376f18f93e45
Eivind Uggedal <eivind@uggedal.com>
2010-03-25 16:26:45 +0100

    License notes of the relpath code taken from the Python standard library.

--- kraftwerk/compat.py
+++ kraftwerk/compat.py
@@ -1,7 +1,8 @@
 """
 os.path.relpath was introduced in Python 2.6. A posix-only implementation
 from the Python 2.6 source is provided here. It's used if we can't import
-an implementation from the system.
+an implementation from the system. The original implementation is released
+under the Python Software Foundation License Version 2
 """
 use_system_version = False
 
