46202491ea111b3ba7a8d88d54ec6674f2cc61e4
Jökull Sólberg Auðunsson <jokull@solberg.is>
2012-07-04 13:39:02 +0000

    Simplified runit runfile

--- kraftwerk/templates/scripts/env.sh
+++ kraftwerk/templates/scripts/env.sh
@@ -1,8 +1,4 @@
 ROOT=/web/{{ project.name }}
-export PATH="$ROOT/bin:$PATH"
-
-PYTHON_VERSION=$(python -c 'import sys; print ".".join(map(str, sys.version_info)[:2])')
-VIRTUALENV_SITEPACKAGES="$ROOT/lib/python$PYTHON_VERSION/site-packages"
 
 {% for key, val in project.environment() -%}
 export {{ key }}="{{ val }}"
--- kraftwerk/templates/scripts/run.sh
+++ kraftwerk/templates/scripts/run.sh
@@ -4,6 +4,7 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
 {% include "scripts/env.sh" %}
+
 export UWSGI_SOCKET=/tmp/uwsgi.{{ project }}.sock
 export UWSGI_MODULE={{ project.config.module }}
 export UWSGI_CALLABLE={{ project.config.callable }}
@@ -12,7 +13,5 @@ export UWSGI_PROCESSES={{ project.config.workers }}
 export UWSGI_MEMORY_REPORT=1
 export UWSGI_HARAKIRI=30
 
-export PYTHONPATH=$VIRTUALENV_SITEPACKAGES
-
 exec 2>&1
-exec uwsgi  --reload-os-env -C --uid web --gid web -H $ROOT
+exec uwsgi --reload-os-env -C --uid web --gid web --venv $ROOT/venv
